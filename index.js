668011704798fd1d02064a90a471a14139553151
John-Mark Sackett <jmsackett91@gmail.com>
2021-08-01 12:43:26 -0400

    tweaked my generate markdown variable to call properly and wrote my write to file function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -91,7 +91,14 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Your README.md file has been created successfully')
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() { }
