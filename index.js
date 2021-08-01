68b59fde2851447b625251cfb09bd1db65622b8a
John-Mark Sackett <jmsackett91@gmail.com>
2021-08-01 12:32:12 -0400

    finished setting up my questions array

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -50,7 +51,43 @@ const questions = [
             return true;
         }
     },
-
+    {
+        type: 'input',
+        message: 'Please type in your E-mail adress',
+        name: 'email',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log('A valid E-mail adress is required!');
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: 'If applicable, give steps needed to install your project for the Installation section.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions for usage and examples of your project in the Usage section.',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'Please select a license for your project',
+        choices: ['afl-3.0', 'apache-2.0', 'ecl-2.0', 'isc', 'mit', 'mpl-2.0', 'ms-pl', 'nsca', 'osl-3.0', 'unlicense'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'If applicable, provide guidelines on how other developers can contribute to this project.',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'If applicable, please provide tests for your project, and explain how to run them.',
+        name: 'tests'
+    }
 ];
 
 // TODO: Create a function to write README file
